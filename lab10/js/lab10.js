0a40cd372c86d59d6ab194726ac43d7a74516e84
rwenzlaf <146970986+rwenzlaf04@users.noreply.github.com>
2023-11-16 16:36:23 -0800

    lab10cganges2

--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -3,7 +3,7 @@
 // Date: 17 November, 2023
 
 function generateRandomText() {
-  const text = "Hey babe, how was youre day";
+  const text = "Hey babe, how was your day. It was alright I ran into a evil sorcerer on my home from work. From under the hood of his tattered brown poncho, his face was distressed, twisted up in knots like the old oak tree in my parents' backyard. He told me that he'd come from the ginger bread house, the one in the woods, the one with glistening gum drops lining the walls and the foot path made of hard candy stones. Only the house wasn't the same as it used to be. The girl who lived in the house was always asleep, and one day not long ago, she woke up. As she was waking up her dad was leaving the house. He blew her a kiss, and the walls of the candy house came falling down. She was walking in a crowded city, like New York City. She tried to talk to the people all around her, but the people were made of paper and they all blew away. Oh, that's nice babe. Im making chicken for dinner.";
   const min = 3;
   const max = 100;
   const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -15,11 +15,11 @@ function generateRandomText() {
 
 // click listener for button
 $("#make-convo").click(function(){
-  $("#output").toggleClass("special");
+  // get new fake dialogue
+  const newText = generateRandomText();
+
+  // append a new div to our output div
+  $("#output").append('<div class="text"><p>' + newText + '</p></div>');
 });
 
-// get new fake dialogue
-const newText = generateRandomText();
 
-// append a new div to our output div
-$("#output").append('<div class="text"><p>' + newText + '</p></div>');
