13a0ac0aa1eb69740eccd0463a7650001b0ea71e
rwenzlaf <146970986+rwenzlaf04@users.noreply.github.com>
2023-12-01 01:29:43 -0800

    lab14changes

--- lab13/js/lab13.js
+++ lab13/js/lab13.js
@@ -3,40 +3,84 @@
 // Date: 27 November, 2023
 
 
+//Give a number and an object that looks like this:
+//      {3: "Crash!", 5: "Clang!", 7 "Meow!"}
+//Loops over the number and outputs the number and the matching text for factors
 
-for (var index=1; index<=200; index++) {
 
-  if (index % 3 === 0 && index % 7 === 0) {
-    console.log ("FizzBuzz!");
+console.log("java script is working");
+
+
+maxFactors = 4;
+outputEl = document.getElementById("output");
+
+//get the values from the user entry and write them as an object
+function getFactorObj () {
+  var factorObj = {};
+  var numId, textId, numValue, textValue;
+  for (var factor=0; factor<maxFactors; factor++) {
+      numId = "num" + factor;
+      textId = "text" + factor;
+      numValue = document.getElementById(numId).value;
+      textValue = document.getElementById(textId).value;
+
+    console.log(factor + ") num:", numValue, "text:", textValue)
+    //if either value if blank, don't use it
+    if (numValue && textValue) {
+      factorObj[numValue] = textValue;
+    }
   }
-  if (index % 3 === 0 && index % 5 === 0) {
-    console.log ("BuzzBoom!") ;
+  return factorObj;
+}
+function outputToPage (str) {
+  newEl = document.createElement ("p");
+  newEl.innerHTML = str;
+  outputEl.appendChild(newEl);
+}
+
+//loops over the number and outputs the number and the corresponding text for factors
+
+function crashClangMeow(maxNums, factorObj) {
+  //iterate over all of the numbers
+
+}
+  for (var num=0; num<maxNums; num++) 
+  debugger;
+  //reset output string
+  var outputStr = "";
+  //iterate over the factors from the html
+  for (var factor in factorObj) {
+      //check to see if this num is a multiple of factor
+      if (num % factor == 0) {
+        //if yes, then add the text to output string
+        outputStr += factorObj [factor];
+      }
   }
-else if (index % 3 === 0) {
-  console.log ("Fizz!") ;
-}
-
-else if (index % 5 === 0) {
-  console.log ("Buzz!");
-}
-else if (index % 7 === 0) {
-  console.log ("Boom!");
-}
-else {
-  console.log (index);
-str = console.log;
-    str+="Fizz!";
-  $("output").append("<p>" + "Fizz!" + "</p>");
-    str += "Buzz!";
-  $("output").append("<p>" + "Buzz!" + "</p>");
-    str += "Boom!";
-  $("output").append("<p>" + "Boom!" + "</p>");
-    str += "FizzBuzz!";
-  $("output").append("<p>" + "FizzBuzz" + "</p>");
-    str += "BuzzBoom!";
-  $("output").append("<p>" + "BuzzBoom" + "</p>");
+  //now if there are words in outputStr, formate them like this " - CrashClang!"
+  if (outputStr) {
+      outputStr = " - " + outputStr + "!";
   }
-
+  outputToPage(num.toString() + outputStr)
+function reportError(str) {
+  outputEl.innerHTML = "<div class='error'>" + str + "</div>";
 }
 
+document.getElementById("submit").addEventListener("click", function () {
+  var max = document.getElementById("max").value;
+  console.log("max:", max)
+  if (! max) {
+    reportError("Yout must provide a maximum");
+    return;
+  }
+  var factorObj = getFactorObj();
+  console.log("factorObj:", factorObj);
+  if (Object.keys(factorObj). length === 0) {
+    reportError("You must provide at least one factor and text");
+    return;
+  }
+  //clear error if there is one
+  outputEl.innerHTML = "";
+  crashClangMeow(max, factorObj);
+  outputStr.classList.add("cols");
+})
 
